4d5593e7b7a73981e886ba85294da8f0aeca36c2
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-10 18:43:46 +0000

    Fixed the " into ' and fixed indented

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function buttonOn() {
-  document.getElementById("paragraph").innerHTML =
-  "Done!"
-document.getElementById("picture").src = "./images/bunny-rabbit.jpg"
-document.getElementById("background").style.backgroundColor = "red" 
-document.getElementById("paragraph").display = "none"
+function buttonOn () {
+  document.getElementById('paragraph').innerHTML = 'Done!'
+  document.getElementById('picture').src =   './images/bunny-rabbit.jpg'
+  document.getElementById('background').style.backgroundColor = 'red' 
+  document.getElementById('paragraph').display = 'none'
 }
