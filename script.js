ef157f0b4b412e5a0ab3f1cea5456e2d5fd28d52
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-10 18:34:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function buttonOn() {
+  document.getElementById("paragraph").innerHTML =
+  "Done!"
+document.getElementById("picture").src = "./images/bunny-rabbit.jpg"
+document.getElementById("background").style.backgroundColor = "red" 
+document.getElementById("paragraph").display = "none"
+}
